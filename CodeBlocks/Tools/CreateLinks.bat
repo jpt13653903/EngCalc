bd42da7efc0194d3db2d8d407034dd102deff355
John-Philip Taylor <jpt13653903@gmail.com>
2014-02-26 08:48:51 +0200

    Library Changes

---
+++ CodeBlocks/Tools/CreateLinks.bat
@@ -0,0 +1,18 @@
+cd "W:\Projects\05\Converter\CodeBlocks\Tools"
+
+mklink /H "Calculator.cpp" "..\..\..\..\Shared\Cpp\Tools\Calculator.cpp"
+mklink /H "Calculator.h" "..\..\..\..\Shared\Cpp\Tools\Calculator.h"
+mklink /H "FileWrapper.cpp" "..\..\..\..\Shared\Cpp\Tools\FileWrapper.cpp"
+mklink /H "FileWrapper.h" "..\..\..\..\Shared\Cpp\Tools\FileWrapper.h"
+mklink /H "JString.cpp" "..\..\..\..\Shared\Cpp\Tools\JString.cpp"
+mklink /H "JString.h" "..\..\..\..\Shared\Cpp\Tools\JString.h"
+mklink /H "LLRBTree.cpp" "..\..\..\..\Shared\Cpp\Tools\LLRBTree.cpp"
+mklink /H "LLRBTree.h" "..\..\..\..\Shared\Cpp\Tools\LLRBTree.h"
+mklink /H "MutEx.cpp" "..\..\..\..\Shared\Cpp\Tools\MutEx.cpp"
+mklink /H "MutEx.h" "..\..\..\..\Shared\Cpp\Tools\MutEx.h"
+mklink /H "UnicodeCodec.cpp" "..\..\..\..\Shared\Cpp\Tools\UnicodeCodec.cpp"
+mklink /H "UnicodeCodec.h" "..\..\..\..\Shared\Cpp\Tools\UnicodeCodec.h"
+mklink /H "XML Wrapper.cpp" "..\..\..\..\Shared\Cpp\Tools\XML Wrapper.cpp"
+mklink /H "XML Wrapper.h" "..\..\..\..\Shared\Cpp\Tools\XML Wrapper.h"
+
+pause
